d342ee1a87cd83db5d968d81d90b8f2484ce3981
StasPotapov <Kapitanoc@yandex.ru>
2019-09-15 22:24:48 +0300

    testing1

--- alert.js
+++ alert.js
@@ -1,2 +1,5 @@
-
-alert('Did I ever tell you what the definition of insanity is?');
+let result = true;
+while (result == true) {
+result = confirm ('Did I ever tell you what the definition of insanity is?');
+}
+alert("Did I ever tell you what the definition of insanity is? Insanity is doing the exact... same fucking thing... over and over again expecting... shit to change... That. Is. Crazy. The first time somebody told me that, I dunno, I thought they were bullshitting me, so, I shot him. The thing is... He was right. And then I started seeing, everywhere I looked, everywhere I looked all these fucking pricks, everywhere I looked, doing the exact same fucking thing... over and over and over and over again thinking 'this time is gonna be different' no, no, no please... This time is gonna be different, I'm sorry, I don't like... The way ... you are looking at me... Okay, Do you have a fucking problem in your head, do you think I am bullshitting you, do you think I am lying? Fuck you! Okay? Fuck you!... It's okay, man. I'm gonna chill, hermano. I'm gonna chill... The thing is... Alright, the thing is I killed you once already... and it's not like I am fucking crazy. It's okay... It's like water under the bridge. Did I ever tell you the definition... of insanity?");
